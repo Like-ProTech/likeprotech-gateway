30e9ed8be80a0e174bb0e1f1733f4ab973b6017e
zakariyaehaourir <zikohaourir@gmail.com>
2026-03-12 06:26:13 +0100

    Implements custom middleware to extract claims

--- Middlewares/ClaimsHeaderMiddleware.cs
+++ Middlewares/ClaimsHeaderMiddleware.cs
@@ -1,6 +1,22 @@
-﻿namespace Likeprotech_gateway.Middlewares
+﻿using System.Security.Claims;
+
+namespace Likeprotech_gateway.Middlewares
 {
     public class ClaimsHeaderMiddleware
     {
+        private readonly RequestDelegate _next;
+        public async Task Invoke(HttpContext context)
+        {
+            if(!(context.User.Identity is null))
+            {
+                Claim? userId = context.User.Claims.FirstOrDefault(c=>c.Type == ClaimsIdentity.DefaultNameClaimType); //NameIdentifier default claim
+                if(userId is not null)
+                {
+                    context.Request.Headers["X-User-Id"] = userId.Value;
+                }
+                
+            }
+            await this._next(context);
+        }
     }
 }
